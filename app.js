d3eb13c6542161944cefe0c4318770eb6365c6d8
Kevin Ng <kehfunn@gmail.com>
2022-08-11 23:15:47 -0400

    final project

--- app.js
+++ app.js
@@ -10,41 +10,33 @@ let matches = 0;
 let shuffledCards = [];
 function shuffle(arr) {
     
-for (let i = 0; i < cards.length; i++) {
+    for (let i = 0; i < cards.length; i++) {
     shuffledCards.push(cards[i])
-
 } 
 
 shuffledCards.sort( () => .5 - Math.random() );
-console.log(shuffledCards)
 
 cards.forEach(card => {card.remove()}) 
 shuffledCards.forEach(card => {
     section.appendChild(card)
 })
-
 }
 
 startButton.addEventListener("click", shuffle)
 
-    let flipped = false; //starts out with all cards unflipped
+    let flipped = false;
     let firstCard, secondCard;
 
 function flipCard() {
     this.classList.add("cards", "flip")
-    // ("class", "cards flip")
 
-    if (!flipped) { //first click
+    if (!flipped) {
         flipped = true
-        firstCard = this //this = our card
-        // console.log(flipped, firstCard)
+        firstCard = this
+
     } else {
-        flipped = false; //second card
+        flipped = false;
         secondCard = this
-        // console.log(firstCard, secondCard)
-        //works to check if its card 1 or card 2
-        // console.log(firstCard.dataset.match)
-        // console.log(secondCard.dataset.match)
 
         if (firstCard.dataset.match === secondCard.dataset.match) {
             firstCard.removeEventListener("click", flipCard)
@@ -57,20 +49,11 @@ function flipCard() {
                 winner.innerHTML = "You win!"
             }
 
-// add id to imgs instead
-//push into open array
-            // correctMatch.push(firstCard)
-            // correctMatch.push(secondCard)
-            
-            //if match - remove event listener so they cannot be clicked again
-            // console.log("removing event listener works")
-            //what if not a match?
         } else {
             setTimeout(() => {
                 console.log("Not a match!")
                 firstCard.classList.remove("flip")
                 secondCard.classList.remove("flip")
-
             }, 1000)
         }
     }
@@ -79,8 +62,6 @@ function flipCard() {
  //citation: https://stackoverflow.com/questions/64852567/html-memory-game-check-if-two-pictures-matched-function
 
 cards.forEach(cards => cards.addEventListener("click", flipCard))
-
-//reset button
 
 
 function resetGame() {
@@ -92,27 +73,9 @@ function resetGame() {
             charles[i].classList.remove("flip")
         }
     }
-    // charles.forEach(card => {
-    //     card.classList.remove("flip")
-    // })
-    console.log(shuffledCards)
     matches = 0
     htmlmatches.innerHTML = `Matches: ${matches}`
     shuffle(shuffledCards)
-    // shuffledCards.forEach(card => {
-    //     flipCard()
-    // })
-    // if (flipped = true) 
-    console.log("it clicks")
 }
 
-
 reset.addEventListener("click", resetGame)
-
-
-
-
-
-
-
-
